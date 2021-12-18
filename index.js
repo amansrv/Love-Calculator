492417fc83ba3292c9134851dadf786257cb0e82
Aman Srivastava <89743268+amansrv@users.noreply.github.com>
2021-12-19 02:57:13 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+// intentionally, i just want to write only JavaScript code behind it.
+// The logic...
+prompt("Enter the name of male candidate");
+prompt("Enter the name of female candidate");
+var num = Math.random() * 100;
+num = Math.floor(num) + 1;
+// console.log(num);
+alert("The love % is between you guys :" + num + "%");
